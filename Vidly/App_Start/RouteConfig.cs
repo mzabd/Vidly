35d5d622c1ea59ad33767e2cebddf25636b6290a
Mohammad Alam <mzabd162@gmail.com>
2018-01-03 13:32:41 -0500

    Changed the bootstrap to lumen and also added custom route

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,6 +12,14 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //for custom route we need to define that route before the default route using MapRoute
+            //name:url:pattern
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new {controller="Movies", Action="ByReleasedDate"},
+                new {year = @"\d{4}", month =@"\d{2}" } //puttinng constrains like the year will be 4 digits
+            );
 
             routes.MapRoute(
                 name: "Default",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie=  new Movie(){Name = "Shrek"};
+
+            //return View(movie);  
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return Content("Hello");
+            return RedirectToAction("Index", "Home", new{page= 1}); //third one is annonymous object
+        }
+
+        //using parameter in the route
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        //optional parameter : retrun a list of movies //movies
+        public ActionResult Index(int? pageIndex, string sortBy) //? to make nullable
+        {
+            //if page index is not specified the movie will displayed in page 1
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            //if sort by is not specified movies will be sort by their name
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content($"pageIndex={pageIndex} & sortBy={sortBy} ");
+        }
+
+        //type: MVCAction4 for code snippet
+        //for custom route: movies/released/{year}/{month}
+        public ActionResult ByReleasedDate(int year, byte month)
+        {
+            return Content(year + " / " + month);
+        }
+    }
+}
